71893b74eb6d58d34443912a3b03fef843e18d9c
Marcus McBride <marcusmcb@gmail.com>
2021-08-18 16:59:39 -0700

    added form input behavior for keydown event

--- src/components/OrderedList.js
+++ src/components/OrderedList.js
@@ -2,11 +2,44 @@ import React from 'react'
 import './OrderedList.css'
 
 const OrderedList = () => {
-    return (
-        <div>
-            <p className='form-list'>FORM LIST GOES HERE</p>
-        </div>
-    )
+  let orderedList = []
+  let orderedListItem = ''
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      console.log('WORKED')
+      orderedList.push(orderedListItem)
+      console.log("ORDERED LIST: ", orderedList)
+      orderedListItem = ''
+      event.currentTarget.value = ''
+    } else if (event.key === 'Backspace') {
+      console.log('BACKSPACE')
+      orderedListItem = orderedListItem.slice(0, -1)
+    } else {
+      orderedListItem += event.key
+      console.log('Event', event.key)
+      console.log('OLI', orderedListItem)
+    }
+  }
+
+  return (
+    <div>
+      <div className='input-row'>
+        <input
+          type='text'
+          className='input-row-element'
+          id='ordered-list-input'
+          onKeyDown={handleKeyDown}
+        ></input>
+        <button type='button' className='input-row-element'>
+          @
+        </button>
+        <button type='button' className='input-row-element'>
+          CL
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default OrderedList
