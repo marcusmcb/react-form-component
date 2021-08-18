6f1731fab2abad307ba20a8d838359de6a6815a2
Marcus McBride <marcusmcb@gmail.com>
2021-08-18 14:58:49 -0700

    updated text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 const App = () => {
   return (
     <div className="App">
-      React Form Component
+      <p>React Form Component</p>
+      <p>Marcus McBride | 2021</p>
     </div>
   );
 }
